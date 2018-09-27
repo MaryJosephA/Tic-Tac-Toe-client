53522cacf712c76bd1ac5f7ebc8f052389e6cc02
Mary Abraham <maryabraham@S01067cbfb1eef3ec.ed.shawcable.net>
2018-09-27 16:29:10 -0400

    committing sept 9/27

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,8 +6,8 @@
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
-$(() => {
-})
+// $(() => {
+// })
 const getWinner = function () {
   const a = 1
   const b = 2
@@ -34,22 +34,47 @@ const getWinner = function () {
     console.log('win')
   } else if (c !== '' && c === e && c === g) {
   }
-
 }
+
+// 1.  Start with a game board of 9 empty spaces
+// const board = ['', '', '', '', '', '', '', '', '']
+//
+// // 2.  Start the current player as x
+// const currentPlayer = 'x'
+//
+// // 3.  Add player x to a position on the board, position 5
+// board[5] = currentPlayer
 
 // set event onclick
 // boxes => all boxes
 // X_or_o => to set x or 0 into the box
-const board = 0
-for(let i = 0; i <= board.length; i++) {
-  board[i] = function(X_orO_)
-  if (X_ or O_ % 2 === 0){
-  board = 'X'
-  gameWinner()
-  X_orO_ += 1;
-} else {
-  board = 'O'
-  gameWinner()
-  X_or_O += 1
-}
+// const board = ['', '', '', '', '', '', '', '', '']
+//
+// // 2.  Start the current player as x
+// let currentPlayer = 'x'
+//
+// // 3.  Add player x to a position on the board, position 5
+// board[5] = currentPlayer
+// getWinner()
+// console.log(currentPlayer)
+// // 4.  Switch player to o
+// currentPlayer = 'o'
+// board[7] = currentPlayer
+// console.log(currentPlayer)
+// 5.  Add player o to position on the board, position 7
+const player1 = 'x'
+const player2 = 'O'
+const board = ['', '', '', '', '', '', '', '', '']
+// const currentPlayer = 'X'
+// board[5] = currentPlayer
+// const count = 0
+for (let i = 1; i <= board.length; i++) {
+  if (board[i] !== 'x' && board[i] !== 'O') {
+    if (board[i] % 2 === 0) {
+      board[i] += 1
+      console.log(player1)
+    } else {
+      console.log(player2)
+    }
+  }
 }
