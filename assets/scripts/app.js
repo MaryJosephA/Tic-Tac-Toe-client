504559f4d933d8d4a828a2ff3c9a58ea53f2413a
Mary Abraham <maryabraham@KesselRunsMBP2.fios-router.home>
2018-10-02 00:41:40 -0400

    committing after modification the project on 10/2/2018

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -9,28 +9,20 @@ const authEvents = require('./auth/events.js')
 // const event = require('./auth/Games/events.js')
 //  const game = require('./auth/board.js')
 $(() => {
-  // $('#sign-up-form').show()
-  $('#main').hide()
-  $('#click1').show()
-  $('.square').click(function () {
-    $('#main').show()
-  })
-  $('#sign-out-button').click(function () {
-    $('#sign-up-form').show()
-    //  $('#sign-in-form').show()
-    // $('#sign-out-button').show()
-    // $('button').hide()
-  })
   $('#sign-up-form').on('submit', authEvents.onSignUp)
   $('#sign-in-form').on('submit', authEvents.onSignIn)
   $('#change-password').on('submit', authEvents.onChangePassword)
   $('#sign-out-button').on('click', authEvents.onSignOut)
-//  $('#sign-in-form').on('submit', event.onGetGames)
-  // $('create-example-form').on('submit', exampleevents.onCrea
+  $('#new-game').on('click', authEvents.onCreateGame)
 })
+// $('#sign-up-form').show()
+$('#main').hide()
+// $('#new-game').on('click', hide)
+$('#click1').show()
 
 const turn = document.getElementById('turn')
 const boxes = document.querySelectorAll('#main div')
+let gameover = false
 let xoro = 0
 const selectWinnerBoxes = function (b1, b2, b3) {
   b1.classList.add('win')
@@ -51,80 +43,117 @@ const selectWinnerBoxes = function (b1, b2, b3) {
 // const tie = function (c1, c2, c3) {
 //   square.onClick.classList.remove('win')
 // }
-
+let count = 0
 const getWinner = function () {
-  const box1 = document.getElementById('box1')
-  const box2 = document.getElementById('box2')
-  const box3 = document.getElementById('box3')
-  const box4 = document.getElementById('box4')
-  const box5 = document.getElementById('box5')
-  const box6 = document.getElementById('box6')
-  const box7 = document.getElementById('box7')
-  const box8 = document.getElementById('box8')
-  const box9 = document.getElementById('box9')
-  if (box1.innerHTML !== '' && box1.innerHTML === box2.innerHTML && box1.innerHTML === box3.innerHTML) {
-    selectWinnerBoxes(box1, box2, box3)
-  } else if (box4.innerHTML !== '' && box4.innerHTML === box5.innerHTML && box4.innerHTML === box6.innerHTML) {
-    selectWinnerBoxes(box4, box5, box6)
-  } else if (box7.innerHTML !== '' && box7.innerHTML === box8.innerHTML && box7.innerHTML === box9.innerHTML) {
-    selectWinnerBoxes(box7, box8, box9)
-  } else if (box1.innerHTML !== '' && box1.innerHTML === box4.innerHTML && box1.innerHTML === box7.innerHTML) {
-    selectWinnerBoxes(box1, box4, box7)
-  } else if (box2.innerHTML !== '' && box2.innerHTML === box5.innerHTML && box2.innerHTML === box8.innerHTML) {
-    selectWinnerBoxes(box2, box5, box8)
-  } else if (box3.innerHTML !== '' && box3.innerHTML === box6.innerHTML && box3.innerHTML === box9.innerHTML) {
-    selectWinnerBoxes(box3, box6, box9)
-  } else if (box1.innerHTML !== '' && box1.innerHTML === box5.innerHTML && box1.innerHTML === box9.innerHTML) {
-    selectWinnerBoxes(box1, box5, box9)
-  } else if (box3.innerHTML !== '' && box3.innerHTML === box5.innerHTML && box3.innerHTML === box7.innerHTML) {
-    selectWinnerBoxes(box3, box5, box7)
-  // } else {
-  //   this.innerHTML = 'tie'
+  count += 1
+  if (count >= 9) {
+    turn.innerHTML = 'draw'
+    turn.innerHTML = 'PLAY'
+  } else {
+    const box1 = document.getElementById('box1')
+    const box2 = document.getElementById('box2')
+    const box3 = document.getElementById('box3')
+    const box4 = document.getElementById('box4')
+    const box5 = document.getElementById('box5')
+    const box6 = document.getElementById('box6')
+    const box7 = document.getElementById('box7')
+    const box8 = document.getElementById('box8')
+    const box9 = document.getElementById('box9')
+    if (box1.innerHTML !== '' && box1.innerHTML === box2.innerHTML && box1.innerHTML === box3.innerHTML) {
+      console.log('win')
+      selectWinnerBoxes(box1, box2, box3)
+      gameover = true
+      turn.innerHTML = 'Invalid Move'
+    } else if (box4.innerHTML !== '' && box4.innerHTML === box5.innerHTML && box4.innerHTML === box6.innerHTML) {
+      selectWinnerBoxes(box4, box5, box6)
+      console.log('win')
+      gameover = true
+      turn.innerHTML = 'Invalid Move'
+    } else if (box7.innerHTML !== '' && box7.innerHTML === box8.innerHTML && box7.innerHTML === box9.innerHTML) {
+      selectWinnerBoxes(box7, box8, box9)
+      console.log('win')
+      gameover = true
+    } else if (box1.innerHTML !== '' && box1.innerHTML === box4.innerHTML && box1.innerHTML === box7.innerHTML) {
+      selectWinnerBoxes(box1, box4, box7)
+      console.log('win')
+      gameover = true
+    } else if (box2.innerHTML !== '' && box2.innerHTML === box5.innerHTML && box2.innerHTML === box8.innerHTML) {
+      selectWinnerBoxes(box2, box5, box8)
+      console.log('win')
+      gameover = true
+    } else if (box3.innerHTML !== '' && box3.innerHTML === box6.innerHTML && box3.innerHTML === box9.innerHTML) {
+      selectWinnerBoxes(box3, box6, box9)
+      console.log('win')
+      gameover = true
+    } else if (box1.innerHTML !== '' && box1.innerHTML === box5.innerHTML && box1.innerHTML === box9.innerHTML) {
+      selectWinnerBoxes(box1, box5, box9)
+      console.log('win')
+      gameover = true
+    } else if (box3.innerHTML !== '' && box3.innerHTML === box5.innerHTML && box3.innerHTML === box7.innerHTML) {
+      selectWinnerBoxes(box3, box5, box7)
+      console.log('win')
+    //  gameOver = true
+    }
   }
 }
-// } else if (box1.innerHTML !== '' && box1.innerHTM !== box2.innerHTML && box1.innerHTML !== box3.innerHTML) {
+// } else {
+//   if (box1.innerHTML !== '' || box1.innerHTM !== box2.innerHTML || box1.innerHTML !== box3.innerHTML) {
+//  turn.innerHTML = 'draw'
+// console.log('draw')
+//   // draw()
+// }
+
+// const draw = function () {
+//   if (box1.innerHTML !== '' && box1.innerHTM !== box2.innerHTML && box1.innerHTML !== box3.innerHTML) {
+//  // console.log('tie')
+//   draw = true
+// }
+
+// } else if (box1.innerHTML == '' && box1.innerHTM !== box2.innerHTML && box1.innerHTML !== box3.innerHTML) {
 // console.log('tie')
 // } else {
 //   this.innerHTML = 'Game Over'
 
 // const boxes = document.querySelectorAll('#main div')
 // let xoro = 0
-
 for (let i = 0; i < boxes.length; i++) {
   boxes[i].onclick = function () {
-    if (this.innerHTML !== 'X' && this.innerHTML !== 'O') {
-    }
-    if (xoro % 2 === 0) {
-      this.innerHTML = 'X'
-      // document.innerHTML('its Os turn')
-      // console.log(xoro)
-      turn.innerHTML = 'O Turn Now'
-      getWinner()
-      //  tie()
-      xoro += 1
+    if (this.innerHTML === 'X' || this.innerHTML === 'O' || gameover === true) {
+      turn.innerHTML = 'Invalid Move'
     } else {
-      this.innerHTML = 'O'
-      turn.innerHTML = 'X Turn Now'
-      getWinner()
-      xoro += 1
+      if (this.innerHTML !== 'X' && this.innerHTML !== 'O') {
+        if (xoro % 2 === 0) {
+          console.log(xoro)
+          this.innerHTML = 'X'
+          turn.innerHTML = 'O Turn Now'
+          getWinner()
+          //  tie()
+          xoro += 1
+        } else {
+          this.innerHTML = 'O'
+          console.log(xoro)
+          turn.innerHTML = 'X Turn Now'
+          getWinner()
+
+          xoro += 1
+        }
+      }
     }
   }
 }
-
 // const gameover = document.querySelectorAll('#main div')
 // for (let i = 0; i < boxes.length; i++) {
 //   boxes[i].onclick.removeClass('main')
 // }
 
-const replay = function () {
-  for (let i = 0; i < boxes.length; i++) {
-    boxes[i].onClick('invalid move')
-    boxes[i].classList.remove('win')
-    boxes[i].innerHTML = ''
-
-    turn.innerHTML = 'Play'
-    turn.style.fontsize = '25px'
-  }
-  return replay
-}
+// const replay = function () {
+//   // const boxes = document.querySelectorAll('#main div')
+//   for (let i = 0; i < boxes.length; i++) {
+//     boxes[i].classList.remove('win')
+//     // boxes[i].onClick('invalid move')
+//     boxes[i].innerHTML = ''
+//
+//     turn.innerHTML = 'Play'
+//     turn.style.fontsize = '25px'
+//   }
 // }
