9914880105e7be508aadf7b2ac7ac0102e5b965d
Mary Abraham <maryabraham@KesselRunsMBP2.fios-router.home>
2018-10-14 19:37:35 -0400

    resubmitting the project 1 modified the below README - List technologies used - Document your planning and tell a story about your development - process and problem-solving strategy. - List unsolved problems which would be fixed in future iterations. - Link to wireframes and user stories.
    1.  When not signed in, user should NOT see “sign out”,
       “new game”, or “change password” -- modified
    2.  When a user sign up fails, they should see a message -- modified
    3.  When a user change password fails,
        they should see a message -- modified
    4.  When a user signs out, it should
        clear ALL the forms  -- modified
    I am not able to solve the issue in the game -- solved by
    hiding the game board

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -1,22 +1,24 @@
 const store = require('../store.js')
-
-const signUpSucess = function () {
-  $('#display-message').html('sign up successful')
+// const Events = require('../auth/events.js')
+// const app = require('assets/scripts/app.js')
+const signUpSuccess = function () {
+  $('#display-message').html('sign-up-success')
   $('#display-message').css('color', 'green')
-  $('#main').show()
-  $('#click').show()
+  $('#sign-up-form').trigger('reset')
+}
+const signUpFailure = function () {
+  $('#display-message').html('sign up failed')
+  $('#display-message').css('color', 'green')
   $('#sign-up-form').hide()
   $('#sign-up-form').trigger('reset')
 }
-
-const signUpFailure = function () {
-  $('display-message').html('Something went wrong please try again')
-  $('display-message').css('color', 'green')
-  $('sign-in-form').trigger('reset')
-}
 const signInSuccess = function (response) {
   $('#display-message').html('sign in successful' + 'ID:' + response.user.id)
-  // $('#main').show()
+  $('#new-game').show()
+  $('#sign-out-button').show()
+  $('#view-game').show()
+  // $('#change-password').show()
+  // $('#main').hide()
   // $('span').html('play')
   // $('#box1').html('').removeClass('win')
   // $('#box2').html('').removeClass('win')
@@ -30,8 +32,6 @@ const signInSuccess = function (response) {
   $('#display-message').css('color', 'green')
   $('#sign-in-form').hide()
   $('#sign-up-form').hide()
-  // $('#click1').show()
-  // $('#click').show()
   $('#sign-out-button').show()
   $('#sign-in-form').trigger('reset')
   store.user = response.user
@@ -45,32 +45,23 @@ const signInFailure = function () {
   $('#display-message').html('oops please try again')
   $('#display-message').css('color', 'red')
   $('#sign-in-form').trigger('reset')
+  $('#sign-up-form').trigger('reset')
+  $('#change-password').trigger('reset')
 }
-const signOutSuccess = function () {
-  // $('#box1').html('').removeClass('win')
-  // $('#box2').html('').removeClass('win')
-  // $('#box3').html('').removeClass('win')
-  // $('#box4').html('').removeClass('win')
-  // $('#box5').html('').removeClass('win')
-  // $('#box6').html('').removeClass('win')
-  // $('#box7').html('').removeClass('win')
-  // $('#box8').html('').removeClass('win')
-  // $('#box9').html('').removeClass('win')
+const signOutSuccess = function (response) {
   $('#display-message').html('Sign Out successful')
-  // $('span').html('PLAY')
-  // $('#box1').html('')
-  // $('#box2').html('')
-  // $('#box3').html('')
-  // $('#box4').html('')
-  // $('#box5').html('')
-  // $('#box6').html('')
-  // $('#box7').html('')
-  // $('#box8').html('')
-  // $('#box9').html('')
+  $('#new-game').hide()
+  $('#view-game').hide()
+  $('#sign-out-button').hide()
+  // $('#display-message').fadeOut(2000)
+  $('#change-password').hide()
+  $('#change-password-header').hide()
   $('#display-message').css('color', 'green')
   $('#main').hide()
   $('#sign-in-form').show()
   $('#sign-up-form').show()
+  $('#sign-in-form').trigger('reset')
+  $('#sign-up-form').trigger('reset')
   $('#sign-up-form').removeClass('hidden')
   $('#sign-in-form').removeClass('hidden')
   $('#change-password').addClass('hidden')
@@ -80,21 +71,21 @@ const signOutFailure = function () {
   $('#display-message').html('Something went wrong, please try again')
   $('#display-message').css('color', 'red')
 }
-const changePasswordSuccess = function (response) {
-  $('#display-message').html('Success change password')
+const changePasswordSuccess = function () {
+  $('#display-message').text('Success change password')
   $('#display-message').css('color', 'green')
   $('#change-password').trigger('reset')
 }
 const changePasswordFailure = function () {
-  $('display-message').html('please try again')
-  $('display-message').css('color', 'green')
-  $('change-password').trigger('reset')
+  $('#display-message').html('please try again')
+  $('#display-message').css('color', 'green')
+  $('#change-password').trigger('reset')
 }
 const onCreateSuccess = function (data) {
   store.game = data.game
   $('#main').show()
-  // $('#main').show()
-  $('#turn').html('PLAY')
+  $('#turn').html('New Game')
+  $('#game-message').hide()
   $('#box1').html('').removeClass('win')
   $('#box2').html('').removeClass('win')
   $('#box3').html('').removeClass('win')
@@ -104,34 +95,61 @@ const onCreateSuccess = function (data) {
   $('#box7').html('').removeClass('win')
   $('#box8').html('').removeClass('win')
   $('#box9').html('').removeClass('win')
+  $('#change-password').show()
+  $('#change-password-header').show()
+  // $('#game-show').show()
+  // $('#box1').click(false)
   $('#main').trigger('reset')
-  $('#message').html('New Game successfully created')
+  // $('#display-message').html('New Game successfully created')
   $('#message').removeClass()
   $('#message').addClass('success')
-  console.log('onCreateSuccess ran. Data is :', data)
+  // console.log('onCreateSuccess ran. Data is :', data)
 }
 //
-// const onCreateFailure = function (error) {
-//   $('#message').text('Error on creating example')
-//   $('#message').removeClass()
-//   $('#message').addClass('failure')
-//   console.error('onCreateFailure ran. Error is :', error)
-// }
-// const onUpdateSuccess = function () {
-//   $('#message').text('Game successfully updated')
-//   $('#message').removeClass()
-//   $('#message').addClass('success')
-//   console.log('Example successfully updated')
-// }
-// const onUpdateFailure = function (error) {
-//   $('#message').text('Error on updating example')
-//   $('#message').removeClass()
-//   $('#message').addClass('failure')
-//   console.error('onUpdateFailure ran. Error is :', error)
-// }
+const onCreateFailure = function (error) {
+  $('#display-message').text('Error on creating example')
+  $('#message').removeClass()
+  $('#message').addClass('failure')
+  console.error('onCreateFailure ran. Error is :', error)
+}
+
+const onUpdateSuccess = function (gameData, r) {
+  store.game = gameData.game
+  // player = store.game.cells[index]
+  $('#turn').html('New Game')
+  // $('#display-message').text('Turn' + gameData.value)
+  // $('#display-message').text('Game successfully updating')
+  // $('#display-message').fadeOut(2000)
+  // $('#display-message').text('update sucess')
+
+  // $('#display-message').html(`Total games played is ${data.games.length}`)
+  $('#display-message').removeClass()
+  $('#display-message').addClass('success')
+  // console.log('Game Successfully Updated')
+}
+
+const onUpdateFailure = function (error) {
+  $('#display-message').text('Error on updating example')
+  $('#display-message').removeClass()
+  $('#display-message').addClass('failure')
+  console.error('onUpdateFailure ran. Error is :', error)
+}
+const onStatSuccess = function (gameData) {
+  $('#display-message').text(`Total games played is ${gameData.games.length}`)
+  $('#display-message').html('Total games played' + ' ' + gameData.games.length)
+  $('#display-message').css('color', 'green')
+  // console.log('onStatSuccess ran. Data is :', data.games.length)
+}
+
+const onStatFailure = function (error) {
+  $('#display-message').text('Please try again')
+  $('#message').removeClass()
+  $('#message').addClass('failure')
+  console.error('onCreateFailure ran. Error is :', error)
+}
 
 module.exports = {
-  signUpSucess,
+  signUpSuccess,
   signUpFailure,
   signInSuccess,
   signInFailure,
@@ -139,8 +157,11 @@ module.exports = {
   signOutFailure,
   changePasswordSuccess,
   changePasswordFailure,
-  onCreateSuccess
-  // onCreateFailure,
-  // onUpdateSuccess,
-  // onUpdateFailure
+  onCreateSuccess,
+  onCreateFailure,
+  onUpdateSuccess,
+  onUpdateFailure,
+  onStatSuccess,
+  onStatFailure
+
 }
